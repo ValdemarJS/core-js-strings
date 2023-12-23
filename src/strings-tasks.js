0c8a04cc88660bfb1025caf795820d1ea235d9d4
ValdemarJS <ierokhinv@gmail.com>
2023-12-23 19:33:46 +0300

    fucking prettier

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -505,65 +505,8 @@ function encodeToRot13(message) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId(value) {
-  const cards = [
-    'A♣',
-    '2♣',
-    '3♣',
-    '4♣',
-    '5♣',
-    '6♣',
-    '7♣',
-    '8♣',
-    '9♣',
-    '10♣',
-    'J♣',
-    'Q♣',
-    'K♣',
-    'A♦',
-    '2♦',
-    '3♦',
-    '4♦',
-    '5♦',
-    '6♦',
-    '7♦',
-    '8♦',
-    '9♦',
-    '10♦',
-    'J♦',
-    'Q♦',
-    'K♦',
-    'A♥',
-    '2♥',
-    '3♥',
-    '4♥',
-    '5♥',
-    '6♥',
-    '7♥',
-    '8♥',
-    '9♥',
-    '10♥',
-    'J♥',
-    'Q♥',
-    'K♥',
-    'A♠',
-    '2♠',
-    '3♠',
-    '4♠',
-    '5♠',
-    '6♠',
-    '7♠',
-    '8♠',
-    '9♠',
-    '10♠',
-    'J♠',
-    'Q♠',
-    'K♠',
-  ];
-  let result;
-  cards.map((item) =>
-    item === value ? result = cards.indexOf(item) : '');
-  return result;
+function getCardId(/* value */) {
+  throw new Error('Not implemented');
 }
 
 module.exports = {
